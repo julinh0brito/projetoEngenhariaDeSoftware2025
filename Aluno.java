5298c158dcb01549ad1eca648da58c4162ec1ee9
Júlio César Brito <160615355+julinh0brito@users.noreply.github.com>
2025-11-15 15:00:26 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -8,6 +8,7 @@ public class Aluno {
     private String matricula;
     private String email;
     private List<Matricula> matriculas = new ArrayList<>();
+    
     public Aluno(int idAluno, String nome, String matricula, String email) {
         this.idAluno = idAluno;
         this.nome = nome;
@@ -18,12 +19,16 @@ public class Aluno {
     // Getters e Setters
     public int getIdAluno() { return idAluno; }
     public void setIdAluno(int idAluno) { this.idAluno = idAluno; }
+    
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
+    
     public String getMatricula() { return matricula; }
     public void setMatricula(String matricula) { this.matricula = matricula; }
+    
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+    
     public List<Matricula> getMatriculas() { return matriculas; }
     public void adicionarMatricula(Matricula matricula) {
         matriculas.add(matricula);
